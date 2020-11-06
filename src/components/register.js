ebba5ebccd4d15964132da1d1e6d042ff9335d66
ikmokhirio <ikmokhirio@mail.ru>
2020-11-06 15:11:07 +0300

    Added onClick handler and state for registration form

--- src/components/register.js
+++ src/components/register.js
@@ -9,6 +9,14 @@ export default class Register extends Component {
     constructor(props) {
         super(props);
 
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            email: "",
+            username: "",
+            password: "",
+            gender: ""
+        }
+
         this.options = [
             {
                 name:"gender",
@@ -23,21 +31,34 @@ export default class Register extends Component {
         ]
     }
 
+    handleClick(event) {
+        event.preventDefault();
+        alert(JSON.stringify(this.state));
+    }
 
     render() {
         return (<form className="input_form">
             <h1>Register</h1>
 
-            <InputField name="Email" type="text" placeholder="E-mail"/>
+            <InputField name="Email" type="text" placeholder="E-mail" onChange={e => {
+                this.setState({email: e.target.value})
+            }}/>
 
-            <InputField name="Username" type="text" placeholder="Username"/>
+            <InputField name="Username" type="text" placeholder="Username" onChange={e => {
+                this.setState({username: e.target.value})
+            }}/>
 
-            <InputField name="Password" type="password" placeholder="Password"/>
+            <InputField name="Password" type="password" placeholder="Password" onChange={e => {
+                this.setState({password: e.target.value})
+            }}/>
 
-            <InputRadio optionName="Пол" options={this.options}/>
+            <InputRadio optionName="Пол" options={this.options} onChange={e => {
+                console.log(e.target.value);
+                this.setState({gender: e.target.value})
+            }}/>
 
             <div className="btn">
-                <button>Register</button>
+                <button onClick={this.handleClick}>Register</button>
             </div>
 
 
