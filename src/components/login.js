d804353988add23bda5ca2d5fb46a862808b0791
ikmokhirio <ikmokhirio@mail.ru>
2020-11-06 15:03:15 +0300

    Added state and onClick event for LOG IN button

--- src/components/login.js
+++ src/components/login.js
@@ -6,19 +6,32 @@ export default class Login extends Component {
 
     constructor(props) {
         super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            username: "",
+            password: ""
+        }
     }
 
+    handleClick(event) {
+        event.preventDefault();
+        alert(JSON.stringify(this.state));
+    }
 
     render() {
         return (<form className="input_form">
             <h1>Login</h1>
 
-            <InputField name="Username" type="text" placeholder="Username"/>
+            <InputField name="Username" type="text" placeholder="Username" onChange={e => {
+                this.setState({username: e.target.value})
+            }}/>
 
-            <InputField name="Password" type="password" placeholder="Password"/>
+            <InputField name="Password" type="password" placeholder="Password" onChange={e => {
+                this.setState({password: e.target.value})
+            }}/>
 
             <div className="btn">
-                <button>LOG IN</button>
+                <button onClick={this.handleClick}>LOG IN</button>
             </div>
 
 
