97ac15b29bac210e32f5767f8a01757eaa514880
Ikmokhirio <Ikmokhirio@mail.ru>
2020-11-22 23:26:27 +0300

    Added button for adding new passwords

--- src/components/passwords/passwords.js
+++ src/components/passwords/passwords.js
@@ -7,6 +7,10 @@ export default class Passwords extends Component {
         super(props);
 
         this.updatePassword = this.updatePassword.bind(this)
+
+        this.state = {
+            edit: false
+        }
     }
 
     updatePassword(passwordData) {
@@ -17,23 +21,31 @@ export default class Passwords extends Component {
     render() {
 
         return (
-            <div className="passwords_container">
 
-                <PasswordCard title="VK password" login="user" password="1234" updatePassword={this.updatePassword}
-                              id={25}/>
+            this.state.edit ? <div className="modal_add_password">
 
-                <PasswordCard title="Mail password" login="user" password="1234567" updatePassword={this.updatePassword}
-                              id={26}/>
+                {/*MODAL WINDOW FOR ADDING PASSWORD*/}
 
-                <PasswordCard title="YouTube password" login="user" password="1234dasd567"
-                              updatePassword={this.updatePassword} id={27}/>
-
-                <div className="add_password_button">
-                    <button>
-                        <h1>ADD NEW PASSWORD</h1>
-                    </button>
                 </div>
-            </div>
+                :
+                <div className="passwords_container">
+
+                    <PasswordCard title="VK password" login="user" password="1234" updatePassword={this.updatePassword}
+                                  id={25}/>
+
+                    <PasswordCard title="Mail password" login="user" password="1234567"
+                                  updatePassword={this.updatePassword}
+                                  id={26}/>
+
+                    <PasswordCard title="YouTube password" login="user" password="1234dasd567"
+                                  updatePassword={this.updatePassword} id={27}/>
+
+                    <div className="add_password_button">
+                        <button onClick={event => this.setState({edit: !this.state.edit})}>
+                            <h1>ADD NEW PASSWORD</h1>
+                        </button>
+                    </div>
+                </div>
 
         )
     }
