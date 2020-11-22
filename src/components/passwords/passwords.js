f79c127d58a97a3f527029748cabe46f55e454b3
Ikmokhirio <Ikmokhirio@mail.ru>
2020-11-22 23:08:43 +0300

    Added 'add new password' button

--- src/components/passwords/passwords.js
+++ src/components/passwords/passwords.js
@@ -19,12 +19,20 @@ export default class Passwords extends Component {
         return (
             <div className="passwords_container">
 
-                <PasswordCard title="VK password" login="user" password="1234" updatePassword={this.updatePassword} id={25}/>
+                <PasswordCard title="VK password" login="user" password="1234" updatePassword={this.updatePassword}
+                              id={25}/>
 
-                <PasswordCard title="Mail password" login="user" password="1234567" updatePassword={this.updatePassword} id={26}/>
+                <PasswordCard title="Mail password" login="user" password="1234567" updatePassword={this.updatePassword}
+                              id={26}/>
 
-                <PasswordCard title="YouTube password" login="user" password="1234dasd567" updatePassword={this.updatePassword} id={27}/>
+                <PasswordCard title="YouTube password" login="user" password="1234dasd567"
+                              updatePassword={this.updatePassword} id={27}/>
 
+                <div className="add_password_button">
+                    <button>
+                        <h1>ADD NEW PASSWORD</h1>
+                    </button>
+                </div>
             </div>
 
         )
