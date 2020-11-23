b11482e2bf8753bcf450958391a802cc50b42548
Ikmokhirio <Ikmokhirio@mail.ru>
2020-11-23 08:10:36 +0300

    Added rendering of popup window

--- src/components/passwords/passwords.js
+++ src/components/passwords/passwords.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import PasswordCard from "./passwordCard";
+import AddPassword from "../input/addPassword";
 
 export default class Passwords extends Component {
 
@@ -7,6 +8,7 @@ export default class Passwords extends Component {
         super(props);
 
         this.updatePassword = this.updatePassword.bind(this)
+        this.edit = this.edit.bind(this)
 
         this.state = {
             edit: false
@@ -18,30 +20,26 @@ export default class Passwords extends Component {
         console.log("UPDATING FROM SERVER")
     }
 
+    edit() {
+        this.setState({edit: !this.state.edit})
+    }
+
     render() {
 
         return (
 
-            this.state.edit ? <div className="modal_add_password">
-
-                {/*MODAL WINDOW FOR ADDING PASSWORD*/}
-
-                </div>
-                :
                 <div className="passwords_container">
 
-                    <PasswordCard title="VK password" login="user" password="1234" updatePassword={this.updatePassword}
-                                  id={25}/>
+                    {this.state.edit ? <AddPassword onClose={this.edit}/> : ""}
 
-                    <PasswordCard title="Mail password" login="user" password="1234567"
-                                  updatePassword={this.updatePassword}
-                                  id={26}/>
+                    <PasswordCard title="VK password" login="user" password="1234" updatePassword={this.updatePassword} id={25}/>
 
-                    <PasswordCard title="YouTube password" login="user" password="1234dasd567"
-                                  updatePassword={this.updatePassword} id={27}/>
+                    <PasswordCard title="Mail password" login="user" password="1234567" updatePassword={this.updatePassword} id={26}/>
+
+                    <PasswordCard title="YouTube password" login="user" password="1234dasd567" updatePassword={this.updatePassword} id={27}/>
 
                     <div className="add_password_button">
-                        <button onClick={event => this.setState({edit: !this.state.edit})}>
+                        <button onClick={this.edit}>
                             <h1>ADD NEW PASSWORD</h1>
                         </button>
                     </div>
