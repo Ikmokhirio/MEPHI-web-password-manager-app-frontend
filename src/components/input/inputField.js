25388147494f20aa85cbcb2f7b7d8f961e818e3d
ikmokhirio <ikmokhirio@mail.ru>
2020-12-14 20:08:28 +0300

    Fixed bug when autocomplete broke input field

--- src/components/input/inputField.js
+++ src/components/input/inputField.js
@@ -10,18 +10,23 @@ export default class InputField extends Component {
     }
 
     inputFocus(event) {
+        if (!event.target.value) this.inputBlur(event);
         this.setState({placeholderClass: "placeholder_focus"});
     }
 
     inputBlur(event) {
-        if(event.target.value) return;
+        if (event.target.value) return;
         this.setState({placeholderClass: "placeholder"});
     }
 
     render() {
         return (
             <div className="input_text">
-                <input name={this.props.name} type={this.props.type} placeholder="" onFocus={this.inputFocus} onBlur={this.inputBlur} onChange={this.props.onChange}/>
+                <input name={this.props.name} type={this.props.type} placeholder="" onFocus={this.inputFocus}
+                       onBlur={this.inputBlur} onChange={(event) => {
+                    this.inputFocus(event)
+                    this.props.onChange(event)
+                }}/>
                 <span className={this.state.placeholderClass}>{this.props.placeholder}</span>
             </div>
         );
